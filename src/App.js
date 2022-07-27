e491916834019a6de4a149424a20478073d26a74
Witold Dziadosz <witold.dziadosz@capgemini.com>
2022-07-27 15:29:43 +0200

    add bigger hello msg

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-red-500">
-      Hello Capgemini!
-    </h1>
-    
+    <>
+    <header className="shadow-sm px-8 py-4 font-semibold text-xl">
+      <h1>Coindashboard</h1>
+    </header>
+    <div className="container md:w-2/3 text-center mx-auto">
+      <h1 className="text-6xl md:text-7xl mt-44 text-indigo-600 font-bold rotate-6 ">Hello ❤️ Capgemini!</h1>
+    </div>
+   
+    </>
   );
 }
 
