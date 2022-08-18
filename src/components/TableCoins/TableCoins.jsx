29ee6118e6e140ea342986e1dd0e599cd0283ac5
fboussad <faouziboussad@gmail.com>
2022-08-18 14:17:26 +0200

    infinite scroll done

--- src/components/TableCoins/TableCoins.jsx
+++ src/components/TableCoins/TableCoins.jsx
@@ -1,16 +1,67 @@
 import TableRow from "../TableRow/TableRow"
 import configData from "../../configData.json"
-import useFetchDataApi from "../../customizedhooks/useFetchDataApi"
+import InfiniteScroll from "react-infinite-scroll-component"
+import { useState } from "react"
+import { useEffect } from "react"
+import axios from "axios"
+import LoaderCoinItems from "../LoaderCoinItems/LoaderCoinItems"
+
+const axiosClient = axios.create({
+    baseURL: configData.BASE_SERVER_URL
+})
 
 function TableCoins({ setCoinName }) {
     // Do our API call using ----- /api/coins
-    const [statusCoins, loaderCoins, coins] = useFetchDataApi(
-        configData.COINS_PATH
-    )
+    const [items, setItems] = useState([])
+    const [page, setPage] = useState(2)
+    const [hasMore, setHasMore] = useState(true)
+    const [loaderCoins, setLoaderCoins] = useState(true)
+    const [statusCoins, setStatusCoins] = useState(102)
+
+    useEffect(() => {
+        ;(async () => {
+            try {
+                const data = await axiosClient.get(configData.COINS_PATH, {
+                    params: {
+                        _page: 1,
+                        _limit: 6
+                    }
+                })
+                setItems(data.data)
+                setStatusCoins(data.status)
+            } catch (error) {
+                setStatusCoins(error)
+            } finally {
+                setLoaderCoins(false)
+            }
+        })()
+    }, [])
+
+    const fetchData = async () => {
+        try {
+            const data = await axiosClient.get(configData.COINS_PATH, {
+                params: {
+                    _page: page,
+                    _limit: 6
+                }
+            })
+            setItems([...items, ...data.data])
+            if (data.data.length < 6 || data.data.length === 0) {
+                setHasMore(false)
+            } else {
+                setPage(page + 1)
+            }
+            setStatusCoins(data.status)
+        } catch (error) {
+            setStatusCoins(error)
+        } finally {
+            setLoaderCoins(false)
+        }
+    }
 
     return (
-        <div id="listCoins" className="mt-2 pt-2 h-screen">
-            <div className="border dark:border-gray-600 h-4/5 w-full p-4 bg-white dark:bg-gray-800 relative overflow-hidden">
+        <div id="listCoins" className="pt-2 h-screen">
+            <div className="border dark:border-gray-600 h-5/6 w-full p-4 pb-6 bg-white dark:bg-gray-800 relative overflow-hidden">
                 <div className="w-full flex items-center justify-between mb-2 border-b pb-1 dark:border-gray-600">
                     <p className="text-gray-800 dark:text-white text-xl">
                         Coins
@@ -26,26 +77,37 @@ function TableCoins({ setCoinName }) {
                     </div>
                 </div>
                 <TableRow type="header" />
-                <div className="overflow-y-scroll h-96 px-1">
+                <div
+                    id="scrollableDiv"
+                    className="overflow-y-scroll h-5/6 px-1"
+                >
                     {!loaderCoins ? (
                         statusCoins !== 200 ? (
-                            <h1>MESSAGE: {statusCoins}</h1>
+                            <h1>MESSAGE: {statusCoins.message}</h1>
                         ) : (
-                            coins.map((coin, index) => {
-                                return (
-                                    <TableRow
-                                        key={index}
-                                        name={coin.name}
-                                        price={coin.current_quote.price}
-                                        urlImage={coin.image_url}
-                                        alt={coin.symbol}
-                                        hour={coin.current_quote.deltas[0]}
-                                        day={coin.current_quote.deltas[1]}
-                                        week={coin.current_quote.deltas[2]}
-                                        setCoinName={setCoinName}
-                                    />
-                                )
-                            })
+                            <InfiniteScroll
+                                dataLength={items.length} //This is important field to render the next data
+                                next={fetchData}
+                                hasMore={hasMore}
+                                scrollableTarget={"scrollableDiv"}
+                                loader={<LoaderCoinItems />}
+                            >
+                                {items.map((coin, index) => {
+                                    return (
+                                        <TableRow
+                                            key={index}
+                                            name={coin.name}
+                                            price={coin.current_quote.price}
+                                            urlImage={coin.image_url}
+                                            alt={coin.symbol}
+                                            hour={coin.current_quote.deltas[0]}
+                                            day={coin.current_quote.deltas[1]}
+                                            week={coin.current_quote.deltas[2]}
+                                            setCoinName={setCoinName}
+                                        />
+                                    )
+                                })}
+                            </InfiniteScroll>
                         )
                     ) : (
                         <h1>LOADING.....</h1>
